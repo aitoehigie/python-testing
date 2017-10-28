d3b5dc1c24ea77be8ceeb8236195d03e6bd43ee7
aitoehigie <aitoehigie@gmail.com>
2017-10-28 02:54:34 +0100

    swap 'assert for assertEqual' as used in the test module

--- test_port1.py
+++ test_port1.py
@@ -9,10 +9,10 @@ class PortfolioTest(unittest.TestCase):
 
     def test_empty(self):
         p = Portfolio()
-        assert p.cost() == 0.0
+        self.assertEqual(p.cost(), 0.0)
 
     def test_buy_two_stocks(self):
         p = Portfolio()
         p.buy("IBM", 100, 176.48)
         p.buy("HPQ", 100, 36.15)
-        assert p.cost() == 21263.0
+        self.assertEqual(p.cost(), 21263.0)
